3017c13ca22de7e21a53b77f70ad96192121f5f8
DCKetellapper <Diederikketellapper@gmail.com>
2017-11-03 10:47:14 +0100

    Hmm wont import

--- Webscrape.R
+++ Webscrape.R
@@ -1,3 +1,11 @@
 # Webscrape File 
+library(rvest)
+library(tidyverse)
 
+b365<- read_html("https://www.bet365.com/#/AC/B1/C1/D13/E122/F16/R1/")
 
+uk_b365 <- html_nodes(b365,".cm-CouponModule")
+
+html <- read_html("http://www.imdb.com/title/tt1490017/")
+cast <- html_nodes(html, "#titleCast .itemprop")
+length(cast)
