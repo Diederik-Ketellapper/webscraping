a9c037ff21af5fa210af0e634508a8bd48d9dbf1
DCKetellapper <Diederikketellapper@gmail.com>
2017-11-03 17:00:14 +0100

    Making some progress: but can't find data on getting text from the buttons. I can just copy the text so should definitely be possible?

--- Webscrape.R
+++ Webscrape.R
@@ -2,10 +2,40 @@
 library(rvest)
 library(tidyverse)
 
-b365<- read_html("https://www.bet365.com/#/AC/B1/C1/D13/E122/F16/R1/")
+html<- read_html("http://sports.williamhill.es/bet_esp/es/betting/t/295/Inglaterra+-+Premier+League.html")
+matches<- html %>% 
+  html_nodes('.rowOdd a span') %>% 
+  html_text()
 
-uk_b365 <- html_nodes(b365,".cm-CouponModule")
+prices<- html %>% 
+html_nodes('.eventprice') %>% 
+html_text()
+prices
 
-html <- read_html("http://www.imdb.com/title/tt1490017/")
-cast <- html_nodes(html, "#titleCast .itemprop")
-length(cast)
+
+html <- read_html("https://www.10bet.com/sports/football/england-premier-league/")
+bet_10 <- html %>% 
+  html_nodes('#bra_1 span') %>% 
+html_text()
+bet_10
+
+tix_link = paste("https://seatgeek.com/new-york-knicks-tickets#events")
+tix_info = tix_link %>% read_html() %>%
+  html_nodes(".event-listing-title span")
+link_date = read_html(tix_link)
+link_date = html_nodes(link_date, ".event-listing-date")
+link_time = read_html(tix_link)
+link_time = html_nodes(link_time, ".event-listing-time")
+link_price = read_html(tix_link)
+link_price = html_node(link_price, ".event-listing-button")
+link_info = read_html(tix_link)
+link_info = html_node(link_info, "span")
+
+#convert to data frame
+ticket_deals = data.frame(deals = html_text(tix_info),
+                          date = html_text(link_date),
+                          time = html_text(link_time),
+                          price = html_text(link_price),
+                          correpsonding_link = html_attr(link_info,"href"))
+
+head(ticket_deals)
