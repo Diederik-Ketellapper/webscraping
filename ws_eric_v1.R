32ddfbc1bacd810f9a0b65f4a3d2dd1642f56d8a
ebgv <eric.brea-garcia@polytechnique.edu>
2017-11-06 22:50:38 +0100

    webscrapping working fine

--- ws_eric_v1.R
+++ ws_eric_v1.R
@@ -12,10 +12,10 @@ library(dplyr)
 # Build the function for Unibet:
 
 my_team_events <- function(team){
-  unibet_url <- glue("https://www.unibet.co.uk/betting#filter/football/all/all/{team}") %>% as.character(unibet_url)
-  read_html(unibet_url)
-  events <- unibet_url %>% 
-    html_nodes('.KambiBC-event-groups-list') %>% 
+  unibet_url <- glue("https://www.unibet.co.uk/betting#filter/football/all/all/{team}") %>% as.character()
+  html <- read_html(unibet_url)
+  events <- html %>% 
+    html_nodes('.KambiBC-event-participants__name') %>% 
     html_text()
   
   return(unibet_url)
