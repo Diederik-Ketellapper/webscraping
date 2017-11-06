ed772c404ef0e826cf63410872263e83912dec23
ebgv <eric.brea-garcia@polytechnique.edu>
2017-11-06 22:11:21 +0100

    Experimenting with phantomJS to avoid java delayed websites

--- ws_eric_v1.R
+++ ws_eric_v1.R
@@ -2,7 +2,9 @@
 
 # Load and install the necessary packages:
 
+library(tidyverse)
 library(jsonlite)
+library(rvest)
 library(glue)
 library(httr)
 library(dplyr)
@@ -10,18 +12,11 @@ library(dplyr)
 # Build the function for Unibet:
 
 my_team_events <- function(team){
-  url <- glue("https://www.unibet.co.uk/betting#filter/football/all/all/{team}")
-  res <- read_html(url)
-  if(res$status_code != 200){
-    print("API error or team not in the database")
-  } else {
-    content(res)
-  }
+  unibet_url <- glue("https://www.unibet.co.uk/betting#filter/football/all/all/{team}") %>% as.character(unibet_url)
+  read_html(unibet_url)
+  events <- unibet_url %>% 
+    html_nodes('.KambiBC-event-groups-list') %>% 
+    html_text()
+  
+  return(unibet_url)
 }
-
-res <- my_team_events(team = "fc_barcelona")
-res %>% class
-res %>% length
-res %>% names
-
-//*[contains(concat( " ", @class, " " ), concat( " ", "KambiBC-mod-outcome__odds", " " ))]
