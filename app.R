4049e77cbb62fd296cd8caa983885faa26d47ffe
DCKetellapper <Diederikketellapper@gmail.com>
2017-11-13 22:32:39 +0100

    Shiny app becoming shiny. Added FAQ and now actually filters.

--- app.R
+++ app.R
@@ -11,9 +11,10 @@ library(shiny)
 library(glue)
 
 
+
 wd<-getwd()
 glue("{wd}/Overview/tests.R") %>% as.character %>% source
-glue("{wd}/all_team_events.R") %>% as.character %>% source
+#glue("{wd}/all_team_events.R") %>% as.character %>% source
 
 
 options(stringsAsFactors = FALSE)
@@ -24,18 +25,27 @@ titlePanel("Which site to bet on for your favorite team?"),
 
 helpText('By: Eric Brea and Diederik Ketellapper '),
 
-sidebarPanel(selectInput("Paris Saint-Germain", "Team:",
+sidebarPanel(selectInput("team", "Team:",
               list(`Ligue1` = ligue1))),
 mainPanel(
   tabsetPanel(
     tabPanel("Plot", tableOutput("data")), 
-    tabPanel("Link"), wellPanel(helpText(a("Click here to bet for your best odds",href="http://www.google.com", target="_blank")))
-)))
+    tabPanel("Help",  
+      h3("FAQ"),
+      p("Q: Are all operating systems supported?"), 
+      p("A: As of now only windows is supported"),
+      p("Q: Will other leagues be added next to Ligue 1"),
+      p("A: As of now there are no plans to add other leagues"),
+      wellPanel(helpText(a("Still need help?",href="mailto:Eric.Brea-Garcia@polytechnique.edu", target="_blank")))
+))))
+
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
-  output$data <- renderTable({df_williamhill
-  })
+server <- function(input, output){
+
+  
+  output$data <- renderTable(read.csv(glue("{wd}/df.csv")) %>% 
+      subset(visitor == input$team | home == input$team))
 }
 
 # Run the application 
